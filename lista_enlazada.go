7f43cd224789371afc7ecf097bf8ab5c17fc8b15
LucasFerreiraUBA <lferreira@fi.uba.ar>
2022-09-29 00:10:26 -0300

    Completadas las funciones Borrarprimero(), Verprimero(), VerUltimo y Largo()

--- lista_enlazada.go
+++ lista_enlazada.go
@@ -17,8 +17,59 @@ type listaEnlazada[T any] struct {
 }
 
 // FUNCIÓN PARA CREAR LA LISTA ENLAZADA
-/*
+
 func CrearListaEnlazada[T any]() Lista[T] {
 	//...
+	lista := new(listaEnlazada[T])
+
+	return lista
+}
+
+func (lista *listaEnlazada[T]) EstaVacia() bool {
+	return lista.primero == nil
+}
+
+func (lista *listaEnlazada[T]) InsertarPrimero(elem T) {
+
+}
+
+func (lista *listaEnlazada[T]) InsertarUltimo(elem T) {
+
+}
+
+func (lista *listaEnlazada[T]) BorrarPrimero() T {
+	if lista.EstaVacia() {
+		panic("lista vacia")
+	}
+	siguiente := lista.primero.siguiente
+	lista.primero.siguiente = nil
+	lista.primero = siguiente
+
+	if lista.primero == nil {
+		lista.ultimo = nil
+	}
+}
+
+func (lista *listaEnlazada[T]) VerPrimero() T {
+	if lista.primero != nil {
+		return lista.primero.dato
+	}
+	panic("lista vacia")
+}
+
+func (lista *listaEnlazada[T]) VerUltimo() T {
+	if lista.ultimo != nil {
+		return lista.ultimo.dato
+	}
+	panic("lista vacia")
+}
+
+func (lista *listaEnlazada[T]) Largo() int {
+	return lista.largo
+}
+
+/*
+func (lista *listaEnlazada[T]) Iterar(visitar func(T) bool){
+
 }
 */
